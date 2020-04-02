e07f9fb0ac9b633dc65855d1b4a877476ce99624
jelena.mladenovic@apexsql.com <>
2020-04-02 14:31:47 +0200

    gh

--- test4/Tables/HumanResources.Department.sql
+++ test4/Tables/HumanResources.Department.sql
@@ -9,6 +9,7 @@ CREATE TABLE [HumanResources].[Department] (
 		[ModifiedDate]     [datetime] NOT NULL,
 		[s]                [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[a]                [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[g]                [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [PK_Department_DepartmentID]
 		PRIMARY KEY
 		CLUSTERED
