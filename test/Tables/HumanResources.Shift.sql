a8946564a659ea74ddad2654b5b2e65ed68fddb5
JelenaMladenovic <jelena.mladenovic@apexsql.com>
2020-04-03 15:11:48 +0200

    Update HumanResources.Shift.sql

--- test/Tables/HumanResources.Shift.sql
+++ test/Tables/HumanResources.Shift.sql
@@ -5,7 +5,7 @@ GO
 CREATE TABLE [HumanResources].[Shift] (
 		[ShiftID]          [tinyint] IDENTITY(1, 1) NOT NULL,
 		[Name]             [dbo].[Name] NOT NULL,
-		[StartTime]        [time](7) NOT NULL,
+		[StartTimecccc]        [time](7) NOT NULL,
 		[EndTime]          [time](7) NOT NULL,
 		[ModifiedDate]     [datetime] NOT NULL,
 		CONSTRAINT [PK_Shift_ShiftID]
