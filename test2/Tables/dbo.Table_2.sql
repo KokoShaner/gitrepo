606905786e5041ae537c0968863e7606abeb9981
JelenaMladenovic <jelena.mladenovic@apexsql.com>
2020-04-06 11:33:58 +0200

    Update dbo.Table_2.sql

--- test2/Tables/dbo.Table_2.sql
+++ test2/Tables/dbo.Table_2.sql
@@ -3,7 +3,7 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table_2] (
-		[dfg]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[dfg_remoteChange]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[dddd]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
