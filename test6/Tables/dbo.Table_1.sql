d74823a0db206360bb361cedee19924dd7ae23c6
jelena.mladenovic@apexsql.com <>
2020-04-06 16:11:36 +0200

    t1c

--- test6/Tables/dbo.Table_1.sql
+++ test6/Tables/dbo.Table_1.sql
@@ -3,10 +3,9 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table_1] (
-		[yx]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[lk]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[p]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[yx]     [nchar](10) NULL,
+		[lk]     [nchar](10) NULL,
+		[p]      [nchar](10) NULL,
+		[z]      [nchar](10) NULL
 ) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[Table_1] SET (LOCK_ESCALATION = TABLE)
 GO
